60c463a45b2ae172e061d1c698d61410c729c551
RubieProductions <47195271+RubieProductions@users.noreply.github.com>
2019-01-30 16:55:17 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client()l
+const config = require('./config.json');
+
+client.config = config;
+
+client.on("ready", () => {
+ console.log(`Loading......`)
+ console.log(`Loading.......`)
+ console.log(`Loading........`)
+ console.log(`Sucessfully loaded.`)
+});
+
+client.on("message", async (message) => {
+ let prefix = client.config.prefix;
+ 
+ if(message.content === prefix + 'website') {
+   let embed = new Discord.RichEmbed()\
+    .setColor('GREEN')
+    .setDescription(`[click me](https://www.rubieproductions.com)`)
+    .setFooter(client.user.username, client.user.displayAvatarURL);
+   message.channel.send(embed);
+ }
+});
+
+client.login(process.env.TOKEN);
