b6bff817473f13a286173a98c5ff232ccee5874f
RubieProductions <47195271+RubieProductions@users.noreply.github.com>
2019-01-31 20:26:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -22,6 +22,15 @@ client.on("message", async (message) => {
     .setFooter(client.user.username, client.user.displayAvatarURL);
    message.channel.send(embed);
  }
+ 
+ if(message.guild.id == '539194177292992563') {
+   if(message.content === prefix + 'add') {
+    let embed = new Discord.RichEmbed()
+     .setColor('RED')
+     .setDescription('Adding bots is currently in development. Please try again at a other time.');
+    message.channel.send(embed);
+   }
+ }
 });
 
 client.login(process.env.TOKEN);
